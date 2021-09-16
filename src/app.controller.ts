ba555e9d6958720a726365f145e3c0ff752900be
akileshrao <akileshrao19@gmail.com>
2021-09-16 09:50:35 +0530

    Auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private authService: AuthService) { }
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  //This handler will only be invoked if it passes the local strategy auth guard
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
